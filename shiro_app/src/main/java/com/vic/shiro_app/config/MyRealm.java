73b603ddb3008f4a8f0941196f945290873a4690
Vic.wong <985695123@qq.com>
2019-05-27 17:41:47 +0800

    添加Shiro配置

---
+++ shiro_app/src/main/java/com/vic/shiro_app/config/MyRealm.java
@@ -0,0 +1,54 @@
+package com.vic.shiro_app.config;
+
+import com.vic.shiro_app.model.SysUser;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * MyRealm
+ *
+ * @author Vic
+ * @date 2019/5/25
+ */
+@Slf4j
+public class MyRealm extends AuthorizingRealm {
+    //授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        SysUser user = (SysUser) principalCollection.getPrimaryPrincipal();
+        Set<String> permission = new HashSet<>(user.getPermissions());
+        simpleAuthorizationInfo.setStringPermissions(permission);
+
+        return simpleAuthorizationInfo;
+    }
+
+    //认证
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        String userName = (String) authenticationToken.getPrincipal();
+        log.info("UserName=>>>>>>>>>>>" + userName);
+        String password =new String ((char[]) authenticationToken.getCredentials());
+        log.info("password=>>>>>>>>>>>" + password);
+        SysUser user = SysUser.builder().userId(1).userName("zhangsan").password("ebec2a46e3a54b78faf9ca2a3f6378b1").salt("2").build();
+        if (null == user) {
+            throw new AuthenticationException();
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(user, user.getPassword(), ByteSource.Util.bytes(user.getSalt()), getName());
+
+        return authenticationInfo;
+    }
+}
---
+++ shiro_app/src/main/java/com/vic/shiro_app/config/ShiroConfig.java
@@ -0,0 +1,73 @@
+package com.vic.shiro_app.config;
+
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * ShiroConfig
+ *
+ * @author Vic
+ * @date 2019/5/25
+ */
+@Configuration
+public class ShiroConfig {
+    @Bean( name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        // 拦截器
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        //配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/auth", "anon");
+        //配置退出过滤器  具体退出代码 shiro 已实现
+        filterChainDefinitionMap.put("/logout", "logout");
+        //		//<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
+        //		//<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/**","authc");
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm( myRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public MyRealm myRealm() {
+        MyRealm myRealm = new MyRealm();
+        myRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return myRealm;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor() {
+
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager());
+
+        return authorizationAttributeSourceAdvisor;
+    }
+
+    @Bean(name = "credentialsMatcher")
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashAlgorithmName("md5");
+        hashedCredentialsMatcher.setHashIterations(2);
+        hashedCredentialsMatcher.setStoredCredentialsHexEncoded(true);
+        return hashedCredentialsMatcher;
+    }
+}
